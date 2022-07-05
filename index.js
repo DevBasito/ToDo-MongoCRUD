9b663bce9e2bea531f56f4d61f33513b5407dd97
DevBasito <vampbaxx@gmail.com>
2022-07-05 13:08:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const port = process.env.PORT;
 
 const mongoose = require("mongoose");
-const connectionString = 'mongodb://localhost:27017/newdb';
+const connectionString = 'mongodb://localhost:27017/Tododb';
 
 mongoose.connect(connectionString, {
     useUnifiedTopology: true
@@ -17,6 +17,29 @@ mongoose.connect(connectionString, {
         console.log("Database Connected")
     }
 
+}
+)
+
+const {Schema} = mongoose;
+// Same as const Schema = mongoose.Schema
+const toDoSchema = new Schema({
+    title: String, 
+    description: String, 
+    timestamp: String
+})
+
+const ToDoCollection = mongoose.model('ToDoCollection', toDoSchema);
+
+ToDoCollection.create({
+    title: "Buy Pouch",
+    description: "Get a new Pouch For your Phone",
+    timestamp: "12:00 pm"
+}, (err, Todos)=>{
+    if (err) {
+        console.log({err})
+    } else {
+        console.log({Todos})
+    }
 }
 )
 
