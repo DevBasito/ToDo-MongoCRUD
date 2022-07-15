89e20584930289fe583ec71739bdfa10ceb8c73c
DevBasito <vampbaxx@gmail.com>
2022-07-15 15:42:51 +0100

    Update todoModel.js

--- Models/todoModel.js
+++ Models/todoModel.js
@@ -3,11 +3,15 @@ const { Schema } = mongoose;
 // Same as const Schema = mongoose.Schema
 
 // Create a schema
-const toDoSchema = new Schema({
-    title: String,
-    description: String,
-    timestamp: String
-})
+const toDoSchema = new Schema(
+    {
+        title: String,
+        description: String
+    },
+    {
+        timestamps: true
+    }
+)
 
 // Initialize a Collection (like Table in sql) in  the DB
 // Document is like a record/row in sql 
